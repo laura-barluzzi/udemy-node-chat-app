f131088aea7349abefd5a4320aa2b4cf46926c89
Laura Barluzzi <laura@koalacoder.com>
2018-01-12 15:42:21 +0000

    adds connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 var express = require('express');
 const port = process.env.PORT || 8080;
 
@@ -6,9 +8,21 @@ const publicPath = path.join(__dirname, '../public');
 
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('New user connected.');
+    
+    socket.on('disconnect', () => {
+        console.log('User disconnected.');
+    });
+});
+
+
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Listening to port ${port}`);
 });
