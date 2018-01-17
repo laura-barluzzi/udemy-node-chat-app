ac3f46d257f2dae872b87ebc7aeb5c865620897d
Laura Barluzzi <laura@koalacoder.com>
2018-01-17 21:27:33 +0000

    formats timestamp with Moment.js

---
+++ playground/time.js
@@ -0,0 +1,7 @@
+var moment = require('moment');
+
+var date = moment();
+
+console.log(date.format('MMM Do YYYY'));
+
+console.log(date.format('h:mm a'));
--- public/js/index.js
+++ public/js/index.js
@@ -8,16 +8,17 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    console.log('newMessage:', message);
+    var timeStamp = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${timeStamp}: ${message.text}`);
     jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
+    var timeStamp = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">See my location</a>');
-    li.text(`${message.from}:`);
+    li.text(`${message.from} ${timeStamp}:`);
     a.attr('href', message.url);
     li.append(a);
     jQuery('#messages').append(li);
