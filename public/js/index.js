9340233cd84787da65cbeb47496f6762c2e3b08e
Laura Barluzzi <laura@koalacoder.com>
2018-01-17 20:59:49 +0000

    adds css style for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -33,10 +33,13 @@ socket.on('newLocationMessage', function (message) {
 
 jQuery('#message-form').on('submit', function (event) {
     event.preventDefault();
+    var messageTextBox = jQuery('#message-text');
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('#message-text').val()
-    }, function () {});
+        text: messageTextBox.val()
+    }, function () {
+        messageTextBox.val('');
+    });
 });
 
 var locationButton = jQuery('#send-location');
@@ -45,13 +48,16 @@ locationButton.on('click', function (event) {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser');
     }
+    locationButton.attr('disabled', 'disabled').text('Sending...');
     
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location.');
     });
 });
--- server/server.js
+++ server/server.js
@@ -20,9 +20,8 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
     
     socket.on('createMessage', (message, callback) => {
-    console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('This is from the server.');
+    callback();
     });
     
     socket.on('createLocationMessage', (coords) => {
