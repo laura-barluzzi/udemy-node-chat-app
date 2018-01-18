7643fdbee4deb25c83d953d10c761033e111408e
Laura Barluzzi <laura@koalacoder.com>
2018-01-18 18:59:26 +0000

    adds autoscroll to bottom of chat

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,19 @@
 var socket = io();
 
+function scrollToBottom () {
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    var scrollTop = messages.prop('scrollTop');
+    var clientHeight = messages.prop('clientHeight');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+    
+    if (scrollTop + clientHeight + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log('connected to server.');
 });
@@ -17,6 +31,7 @@ socket.on('newMessage', function (message) {
         createAt: timeStamp
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -29,6 +44,7 @@ socket.on('newLocationMessage', function (message) {
         url: message.url
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (event) {
